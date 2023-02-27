b62fc31390ff1a1b7405bb1edc8ca564439cdfdb
André Abadesso <andre.abadesso@gmail.com>
2023-02-27 12:14:23 -0300

    feat: initial commit

---
+++ packages/snap/src/index.ts
@@ -0,0 +1,90 @@
+import {getNetwork} from './bitcoin/getNetwork';
+import {Snap, MetamaskBTCRpcRequest} from './interface';
+import {
+  getHathorXPubKey,
+  getExtendedPublicKey,
+  getAllXpubs,
+  signPsbt,
+  getMasterFingerprint,
+  manageNetwork,
+  validateRequest,
+  saveLNDataToSnap,
+  getLNDataFromSnap,
+  signLNInvoice,
+} from './rpc';
+import { SnapError, RequestErrors } from './errors';
+
+declare let snap: Snap;
+
+export type RpcRequest = {
+  origin: string;
+  request: MetamaskBTCRpcRequest;
+};
+
+export const onRpcRequest = async ({origin, request}: RpcRequest) => {
+  await validateRequest(snap, origin, request);
+
+  switch (request.method) {
+    // getXPubKey
+    // getAuthXPub
+    // getWalletId
+    // signMessage
+    case 'htr_getxpubkey':
+      return getHathorXPubKey(
+        origin,
+        snap,
+      );
+    case 'btc_getPublicExtendedKey':
+      return getExtendedPublicKey(
+        origin,
+        snap,
+        request.params.scriptType,
+        getNetwork(request.params.network),
+      );
+    /* case 'btc_getAllXpubs':
+      return getAllXpubs(
+        origin,
+        snap,
+      ); */
+    case 'btc_signPsbt':
+      const psbt = request.params.psbt;
+      return signPsbt(
+        origin,
+        snap,
+        psbt,
+        request.params.network,
+        request.params.scriptType,
+      );
+    case 'btc_getMasterFingerprint':
+      return getMasterFingerprint(snap);
+    case 'btc_network':
+      return manageNetwork(
+        origin,
+        snap,
+        request.params.action,
+        request.params.network,
+      );
+    case 'btc_saveLNDataToSnap':
+      return saveLNDataToSnap(
+        origin,
+        snap,
+        request.params.walletId,
+        request.params.credential,
+        request.params.password,
+      );
+    case 'btc_getLNDataFromSnap':
+      return getLNDataFromSnap(
+        origin,
+        snap,
+        {
+          key: request.params.key,
+          ...(request.params.walletId && {walletId: request.params.walletId}),
+          ...(request.params.type && {type: request.params.type}),
+        }
+      );
+    case 'btc_signLNInvoice':
+      return signLNInvoice(origin, snap, request.params.invoice);
+    default:
+      throw SnapError.of(RequestErrors.MethodNotSupport);
+  }
+};
---
+++ packages/snap/src/rpc/getHathorXPubKey.ts
@@ -0,0 +1,58 @@
+import { panel, text, heading } from '@metamask/snaps-ui';
+import * as bip32 from 'bip32';
+import { BIP32Interface } from 'bip32';
+import { ScriptType, SLIP10Node, Snap } from '../interface';
+import { trimHexPrefix } from '../utils';
+import { RequestErrors, SnapError } from '../errors';
+import { convertXpub } from '../bitcoin/xpubConverter';
+import { encode, decode } from "bs58check";
+
+export const CRYPTO_CURVE = 'secp256k1';
+
+const hathorNetwork = {
+  messagePrefix: '\x18Hathor Signed Message:\n',
+  bech32: 'ht',
+  bip32: { public: 76067358, private: 55720709 },
+  pubKeyHash: 40,
+  scriptHash: 100,
+  wif: 128
+};
+
+export async function getHathorXPubKey(origin: string, snap: Snap): Promise<{ xpub: string }> {
+  await snap.request({
+    method: 'snap_dialog',
+    params: {
+      type: 'Confirmation',
+      content: panel([
+        heading('Access your extended public key'),
+        text(`${origin} is trying to access your Hathor extended public key.`),
+      ]),
+    },
+  });
+
+  const path = [
+    "m",
+    "44'",
+    "280",
+    "0'"
+  ];
+
+  const slip10Node = await snap.request({
+    method: "snap_getBip32Entropy",
+    params: {
+      path,
+      curve: CRYPTO_CURVE
+    },
+  }) as SLIP10Node
+
+  const privateKeyBuffer = Buffer.from(trimHexPrefix(slip10Node.privateKey), 'hex')
+  const chainCodeBuffer = Buffer.from(trimHexPrefix(slip10Node.chainCode), 'hex')
+  const node: BIP32Interface = bip32.fromPrivateKey(privateKeyBuffer, chainCodeBuffer, hathorNetwork)
+
+  let data = decode(node.neutered().toBase58());
+  data = data.slice(4);
+  data = Buffer.concat([Buffer.from('0488b21e', 'hex'), data]);
+  const xpub = encode(data);
+
+  return { xpub };
+}
